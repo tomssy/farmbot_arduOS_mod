e2012f7967ca8cf0c2bd39073f2e4902ed80912e
tomssy <522897675@qq.com>
2017-03-12 10:01:07 +0800

    Update chaincode_finished.go

--- finished/chaincode_finished.go
+++ finished/chaincode_finished.go
@@ -468,9 +468,9 @@ func (t *SimpleChaincode) update_weather(stub shim.ChaincodeStubInterface, args 
 		json.Unmarshal(InsuranceAsBytes, &Insurances) //un stringify it aka JSON.parse()
 		bad_count := 0
 		wl := len(update_farm.WeatherIndex)
-		for i := wl-3; i < wl; i++ {
-			Weather := update_farm.WeatherIndex[i] 
-			if Weather. == "Rainy" {
+		for i := wl - 3; i < wl; i++ {
+			Weather := update_farm.WeatherIndex[i]
+			if Weather.Name == "Rainy" {
 				bad_count += 1
 			}
 			if Weather.Name == "Sunny" {
